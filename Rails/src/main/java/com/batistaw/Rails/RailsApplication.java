a3cffd852f5c94ec7bb129a4cb76668d2d7ab846
Batistaw <wgoncalvesbatistaa@gmail.com>
2026-06-12 23:57:41 -0400

    Initial commit: Estrutura de pasta FullStack

---
+++ Rails/src/main/java/com/batistaw/Rails/RailsApplication.java
@@ -0,0 +1,13 @@
+package com.batistaw.Rails;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RailsApplication.class, args);
+	}
+
+}
---
+++ Rails/src/test/java/com/batistaw/Rails/RailsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.batistaw.Rails;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RailsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
